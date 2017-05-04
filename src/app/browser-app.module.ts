8246e6c06f109acbd2c4ef4ae6d3a9d9ab59cf95
Sam Vloeberghs <sam.vloeberghs@gmail.com>
2017-05-04 18:36:54 +0200

    updated example as to Marks first comment

--- src/app/browser-app.module.ts
+++ src/app/browser-app.module.ts
@@ -3,15 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { BrowserTransferStateModule } from '../modules/transfer-state/browser-transfer-state.module';
+import { REQUEST } from './request';
+
+export function getRequest() {
+  return {cookie: document.cookie};
+}
 
 @NgModule({
-	bootstrap: [ AppComponent ],
-	imports: [
+  bootstrap: [AppComponent],
+  imports: [
     BrowserModule.withServerTransition({
       appId: 'my-app-id'
     }),
     BrowserTransferStateModule,
     AppModule
-	]
+  ],
+  providers: [
+    {
+      provide: REQUEST,
+      useFactory: (getRequest)
+    }
+  ]
 })
-export class BrowserAppModule {}
+export class BrowserAppModule {
+}
--- src/main.browser.ts
+++ src/main.browser.ts
@@ -4,21 +4,5 @@ import 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAppModule } from './app/browser-app.module';
-import { REQUEST, RESPONSE } from '@nguniversal/express-engine/dist/src/tokens';
-import { CompilerOptions } from '@angular/core';
 
-// extra providers
-const compilerOptions: CompilerOptions = {
-  providers: [
-    {
-      provide: REQUEST,
-      useValue: {}
-    },
-    {
-      provide: RESPONSE,
-      useValue: {}
-    }
-  ]
-};
-
-platformBrowserDynamic().bootstrapModule(BrowserAppModule, compilerOptions);
+platformBrowserDynamic().bootstrapModule(BrowserAppModule);
